6e0507611c717dfa52183b1dba1982827f0d2a52
Vegard Holter <vegard.holter@gmail.com>
2013-10-23 17:25:43 +0200

    renaming level

--- src/problemSolver/Main.java
+++ src/problemSolver/Main.java
@@ -67,7 +67,7 @@ public class Main {
 										   {0, 3, 4, 0, 0, 1, 0, 0, 8},
 										   {0, 0, 0, 5, 0, 0, 6, 0, 0}};
 	
-	//FROM WEBSUDOKU - MEDIUM
+	//FROM WEBSUDOKU - EVIL
 	public static int[][] sudokuPuzzle3 = {{0, 0, 9, 0, 7, 0, 3, 0, 0},
 										   {0, 4, 0, 8, 0, 0, 0, 0, 0},
 										   {2, 7, 0, 0, 0, 5, 0, 0, 0},
