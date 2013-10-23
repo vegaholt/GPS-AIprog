502ac107c846428752cfba71b56c05508adf2ff1
Vegard Holter <vegard.holter@gmail.com>
2013-10-23 16:13:46 +0200

    Clean up KQueenStateManager + Added puzzles to main

--- src/problemSolver/statemanager/KQueenStateManager.java
+++ src/problemSolver/statemanager/KQueenStateManager.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 
 public class KQueenStateManager extends StateManager {
 
-	public final int k;
-	private int[][] minConflicts;
+	public final int k; // Number of queens
+	private int[][] minConflicts; // Holds conflicts relative to each cell on the board
 
-	// Constructor blank state
+	/**
+	 * Constructs a new state for KQueen
+	 * 
+	 * @param k board size
+	 */
 	public KQueenStateManager(int k) {
 		super();
 		this.k = k;
@@ -15,22 +19,26 @@ public class KQueenStateManager extends StateManager {
 		setValuesSize(k);
 	}
 
-	// Initiate state
+	/**
+	 * Initiate a blank state by randomly placing a Queen on each row
+	 */
 	public void initState() {
+
+		// The value array holds the Queen index
 		for (int i = 0; i < k; i++) {
 			values[i] = (int) (Math.random() * k);
 		}
-		
-		//Init conflicts
+
+		// Initiates conflicts
 		this.minConflicts = new int[k][k];
 		for (int i = 0; i < k; i++) {
 			addK(i, values[i]);
 		}
 	}
 
-	// public void revertTo
-
-	// Display solution
+	/**
+	 * Displays the board, number of queens, number of conflicts and the state value
+	 */
 	public void printState() {
 		if (k <= 0) {
 			for (int i = 0; i < k; i++) {
@@ -43,12 +51,16 @@ public class KQueenStateManager extends StateManager {
 				System.out.println("Queen index: " + values[i]);
 			}
 		}
-		System.out.println(k+"-Queens");
+		System.out.println(k + "-Queens");
 		System.out.println("Conflicts: " + calculateConflicts());
 		System.out.println("Value: " + getStateValue());
 	}
 
-	// Calculate conflicts
+	/**
+	 * Detects conflicts for each slot on the board
+	 * 
+	 * @return sum of conflicts
+	 */
 	private int calculateConflicts() {
 		for (int i = 0; i < conflicts.length; i++) {
 			conflicts[i] = 0;
@@ -68,12 +80,24 @@ public class KQueenStateManager extends StateManager {
 		return newConflict;
 	}
 
-	// Get state value
+	/**
+	 * Calculates the state value. 1 - (number of conflicts / max number of conflicts)
+	 * Maximum number of conflicts is calculated to be 2*k - 3, representing that two columns holds all the queens,
+	 * with a queen on every odd numbered index on the first column, and even numbered index on the second column.
+	 * 
+	 * @return state value
+	 */
 	public double getStateValue() {
 		return 1 - (double) calculateConflicts() / (2 * k - 3);
 	}
-	
-	// Relocate conflicted K
+
+	/**
+	 * Swap is overwritten from the StateManager. 
+	 * Swap will find a row where the Queen is involved in at least one conflict. 
+	 * Then it will remove the Queen and search for the most optimal new placement. 
+	 * If more than one such place exists, the placement of the Queen will be random among these. 
+	 * The Queen is removed and placed using private methods, removeK(row, column) and addK(row) 
+	 */
 	@Override
 	public void swap() {
 
@@ -110,12 +134,13 @@ public class KQueenStateManager extends StateManager {
 		// Place K in this column
 		addK(row, bestIndex);
 
-		// Recalculate sum of conflicts and state value
-//		calculateConflicts();
-//		calculateStateValue();
 	}
 
-	// Remove K from the grid
+	/**
+	 * Removes the Queen on a given row in order to recalculate conflicts
+	 * 
+	 * @param row
+	 */
 	private void removeK(int row) {
 
 		int column = values[row];
@@ -152,18 +177,24 @@ public class KQueenStateManager extends StateManager {
 		minConflicts[row][column] += 1;
 	}
 
-	// Place K on the grid
+	/**
+	 * Places a Queen on the given row and column and recalculate the array of
+	 * conflicts
+	 * 
+	 * @param row
+	 * @param column
+	 */
 	private void addK(int row, int column) {
 
 		values[row] = column;
 
-		// Add conflict on columns
+		// Add conflict to columns
 		for (int j = 0; j < k; j++) {
 			minConflicts[j][column] += 1;
 		}
 		minConflicts[row][column] -= 1;
 
-		// Add conflict on diagonal-right
+		// Add conflict to diagonal-right
 		int startColumn = Math.max(0, column - row);
 		int startRow = Math.max(0, row - column);
 
@@ -175,7 +206,7 @@ public class KQueenStateManager extends StateManager {
 		}
 		minConflicts[row][column] -= 1;
 
-		// Add conflict on diagonal-left
+		// Add conflict to diagonal-left
 		startColumn = Math.min(k - 1, row + column);
 		startRow = Math.max(0, row - (k - 1 - column));
 
